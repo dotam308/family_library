1626fe51ad554ba07015e35a4ac073b938f2a7ed
Đỗ Đức Tâm <dotam308@gmail.com>
2021-08-10 01:23:23 +0700

    fix config

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    // 'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => 'public',
 
     /*
     |--------------------------------------------------------------------------
@@ -37,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
